903f37ee7ca22213bf5873aebfbc9d7d6d1e133f
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-04 12:48:39 +0500

    mock-server-data.type.ts

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,9 @@
+import {Coordinates} from './coordinates.type';
+
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  previewImages: string[];
+  images: string[];
+  locations: Coordinates[];
+};
