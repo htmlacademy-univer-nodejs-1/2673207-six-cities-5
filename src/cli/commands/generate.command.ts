39e169ae59ee8f0310f605d5f0d10e043364ad81
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-05 11:10:44 +0500

    generate command

---
+++ src/cli/commands/generate.command.ts
@@ -0,0 +1,48 @@
+import got from "got";
+
+import { MockServerData } from "../../shared/types";
+import { Commander } from "./command.interface";
+import { TSVOfferGenerator } from "../../shared/libs/offer-generator/tsv-offer-generator";
+import { appendFile } from "node:fs/promises";
+
+export class GenerateCommand implements Commander {
+  private initialData!: MockServerData;
+
+  private async load(url: string) {
+    try {
+      this.initialData = await got.get(url).json();
+    } catch {
+      throw new Error(`Can't load data from ${url}`);
+    }
+  }
+
+  public getName(): string {
+    return '--generate';
+  }
+
+  private async write(filepath: string, offerCount: number) {
+    const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    for (let i = 0; i < offerCount; i++) {
+      await appendFile(
+        filepath,
+        `${tsvOfferGenerator.generate()}\n`,
+        { encoding: 'utf-8' }
+      );
+    }
+  }
+
+  public async execute(...params: string[]): Promise<void> {
+    const [count, filePath, url] = params;
+    const offerCount = Number.parseInt(count, 10);
+
+    try {
+      await this.load(url);
+      await this.write(filePath, offerCount)
+    } catch (error: unknown) {
+      console.error("Can't generate data");
+      if (error instanceof Error) {
+        console.error(error.message);
+      }
+    }
+  }
+}
