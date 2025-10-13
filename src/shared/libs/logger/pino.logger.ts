b41ea6ceb6b0d0b0376da06efbe4107bd51f9f46
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-13 22:09:27 +0500

    done

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,25 +1,50 @@
-import { Logger as PinoInstance, pino} from "pino";
+import { Logger as PinoInstance, pino, transport } from 'pino';
+import { injectable } from 'inversify';
+import { resolve } from 'node:path';
 import { Logger } from './logger.interface.js';
+import { getCurrentModuleDirectoryPath } from '../../helpers/index.js';
 
+@injectable()
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
   constructor() {
-    this.logger = pino();
-  }
-  public info(message: string, ...args: unknown[]): void {
-    this.logger.info({}, message, ...args);
+    const modulePath = getCurrentModuleDirectoryPath();
+    const logFilePath = 'logs/rest.log';
+    const destination = resolve(modulePath, '../../../', logFilePath);
+
+    const multiTransport = transport({
+      targets: [
+        {
+          target: 'pino/file',
+          options: { destination },
+          level: 'debug'
+        },
+        {
+          target: 'pino/file',
+          level: 'info',
+          options: {},
+        }
+      ],
+    });
+
+    this.logger = pino({}, multiTransport);
+    this.logger.info('Logger created…');
   }
 
-  public warn(message: string, ...args: unknown[]): void {
-    this.logger.warn({}, message, ...args);
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug({...args}, message);
   }
 
   public error(message: string, error: Error, ...args: unknown[]): void {
     this.logger.error(error, message, ...args);
   }
 
-  public debug(message: string, ...args: unknown[]): void {
-    this.logger.debug({}, message, ...args);
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info({...args}, message);
+  }
+
+  public warn(message: string, ...args: unknown[]): void {
+    this.logger.warn({...args}, message);
   }
 }
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export * from './housing-type.enum.js';
 export * from './offer.type.js';
 export * from './user.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
