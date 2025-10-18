5553b6013b417a02cb4e79620a5b6166ad9acc61
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-18 13:17:44 +0500

    createdAt, updatedAt

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,7 +1,10 @@
 import { Schema, Document, model } from 'mongoose';
 import { User } from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
   email: String,
