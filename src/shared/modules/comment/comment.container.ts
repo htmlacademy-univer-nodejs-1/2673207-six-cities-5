5792a121a32c6fd50d392312b116146002163c25
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-10-26 12:38:15 +0500

    container

--- src/shared/modules/comment/comment.container.ts
+++ src/shared/modules/comment/comment.container.ts
@@ -1,19 +1,17 @@
-import { Container } from 'inversify';
+import { ContainerModule, ContainerModuleLoadOptions } from 'inversify';
 import { types } from '@typegoose/typegoose';
 import { Component } from '../../types/index.js';
 import { CommentEntity, CommentModel } from './comment.entity.js';
 import { DefaultCommentService } from './default-comment.service.js';
 import { CommentService } from './comment-service.interface.js';
 
-export function createCommentContainer() {
-  const commentContainer = new Container();
-
-  commentContainer.bind<CommentService>(Component.CommentService)
+export const commentContainer: ContainerModule = new ContainerModule(
+  (options: ContainerModuleLoadOptions) => {
+    options.bind<CommentService>(Component.CommentService)
     .to(DefaultCommentService)
     .inSingletonScope();
 
-  commentContainer.bind<types.ModelType<CommentEntity>>(Component.CommentModel)
+    options.bind<types.ModelType<CommentEntity>>(Component.CommentModel)
     .toConstantValue(CommentModel);
 
-  return commentContainer;
-}
+  });
