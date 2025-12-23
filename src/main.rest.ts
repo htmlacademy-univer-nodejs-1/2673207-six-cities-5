fc7174990cf382d255222b1ebe97b63ecb1f7cc0
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-12-23 21:05:27 +0500

    add comment service

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,6 +5,7 @@ import { Component } from './shared/types/index.js';
 import { createRestApplicationContainer } from './rest/rest.container.js';
 import { createUserContainer } from './shared/libs/modules/user/index.js';
 import { createOfferContainer } from './shared/libs/modules/offer/offer.container.js';
+import { createCommentContainer } from './shared/libs/modules/comment/comment.container.js';
 
 
 async function bootstrap() {
@@ -12,6 +13,7 @@ async function bootstrap() {
     createRestApplicationContainer(),
     createUserContainer(),
     createOfferContainer(),
+    createCommentContainer(),
   );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
---
+++ src/shared/libs/modules/comment/comment.container.ts
@@ -0,0 +1,13 @@
+import { Container } from "inversify";
+import { Component } from "../../../types/index.js";
+import { CommentEntity, CommentModel, CommentService, DefaultCommentService } from "./index.js";
+import { types } from "@typegoose/typegoose";
+
+export function createCommentContainer() {
+  const commentContainer = new Container();
+
+  commentContainer.bind<CommentService>(Component.CommentService).to(DefaultCommentService).inSingletonScope();
+  commentContainer.bind<types.ModelType<CommentEntity>>(Component.CommentModel).toConstantValue(CommentModel);
+
+  return commentContainer;
+}
