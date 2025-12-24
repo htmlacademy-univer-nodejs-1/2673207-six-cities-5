f52b412e57a2c338e2d0f7e4b4a4a10a2a29b2eb
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-12-24 14:07:35 +0500

    a

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -4,22 +4,30 @@ import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import express, { Express } from 'express';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-  ) {}
+  ) {
+    this.server = express();
+  }
 
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
 
     this.logger.info('Init database…');
     await this._initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('try to init server...');
+    await this._initServer();
+    this.logger.info('Init server completed');
   }
 
   private async _initDb() {
@@ -32,5 +40,10 @@ export class RestApplication {
     );
 
     return this.databaseClient.connect(mongoUri);
+  }
+
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
   }
 }
