e17ef6e29b3aab2b3e7cc7a716a9541ea7741b1d
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-12-25 17:01:49 +0500

    express.static

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -74,6 +74,10 @@ export class RestApplication {
 
   private async _initMiddleware() {
     this.server.use(express.json());
+    this.server.use(
+      '/upload',
+      express.static(this.config.get('UPLOAD_DIRECTORY'))
+    );
   }
 }
 
