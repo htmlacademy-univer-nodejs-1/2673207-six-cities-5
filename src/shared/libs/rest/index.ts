ff73033b51bcf4819f303d017f97e3dd0bbb9643
JohnyCeleron <147262524+JohnyCeleron@users.noreply.github.com>
2025-12-25 15:11:26 +0500

    validate object id middleware

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -8,3 +8,4 @@ export * from './exception-filter/exception-filter.interface.js';
 export * from './exception-filter/app-exception-filter.js';
 export * from './errors/index.js';
 export * from './middleware/middleware.interface.js';
+export * from './middleware/validate-objectid.middleware.js';
